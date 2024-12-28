c1b02bee336c88ab951b0158b05362b73e173ebe
Aiman <aimanzulbahari@gmail.com>
2024-12-28 21:57:05 +0800

    -Add new file call variables.php

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Hello World</h1>
+    <!-- PHP Code Goes Here -->
     <?php
+    // PHP Code Goes Inside Here
     echo "Hello World!";
     // Check PHP Version
     echo "<br>PHP Version: " . phpversion();
+    ?>
+
+    <!-- PHP Are Case Sensitive -->
+    <!-- Will Get An Error because it treated as different variables -->
+    <?php
+    $color = "red";
+        echo "My car is " . $color . "<br>";
+        echo "My house is " . $COLOR . "<br>";
+        echo "My boat is " . $coLOR . "<br>";
     ?>
 </body>
 </html>
---
+++ variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Create Variables in PHP -->
+    <h2 style="text-align: center;">Declare a variable</h2>
+    <?php
+    // Starts by declaring a variable with dollar sign ($) and come with varible naem
+    ?>
+</body>
+</html>
